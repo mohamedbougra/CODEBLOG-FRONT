0e818a6df4577c56b7864e4e598d03ef30f49ac3
mohamed bougra <mohamed.bougra@edu.uca.ma>
2023-04-05 17:11:09 +0000

    update home page

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,26 +1,61 @@
 <template>
-  <div class="row">
-    <div v-for="topic in topics" :key="topic.id" class="col-md-6 col-lg-4 mb-4">
+  <div class="row px-4">
+    <div
+      v-for="topic in topics"
+      :key="topic.id"
+      class="col-md-12 col-lg-6 mb-6 cards-style"
+    >
       <div class="card">
-        <img :src="topic.imageUrl" class="card-img-top" alt="Topic image" />
-        <div class="card-body">
-          <h5 class="card-title">{{ topic.name }}</h5>
-          <p class="card-text">{{ topic.description }}</p>
-          <router-link :to="topic.url" class="btn addArticle"
-            >Explore {{ topic.name }}</router-link
+        <div class="position-relative">
+          <img
+            loading="lazy"
+            class="card-img-top rounded-4"
+            :src="topic.imageUrl"
+            alt="Build CRUD APIs Using Apollo Server(Graphql), MongoDB and Node.Js"
+          />
+          <ul
+            class="list-inline card-badge"
+            style="position: absolute; top: 0; left: 0"
           >
-          <div class="text-muted"><p>Last updated 3 mins ago</p></div>
-
-          <router-link to="/profile">{{ topic.autour }}</router-link>
-          <div>
-            <button
-              type="input"
-              class="btn btn-light btn-sm"
-              style="border-radius: 35px; background: #e3e3e3"
+            <li class="list-inline-item">
+              <a href="/categories/node" class="login">{{ topic.topic }}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="card-body">
+          <h3 class="h3 card-title">
+            <a
+              class="text-decoration-none text-reset tit"
+              href="/tutorial-graphql-apollo-server-nodejs-mongodb/"
+              >{{ topic.name }}</a
             >
-              {{ topic.topic }}
-            </button>
-          </div>
+          </h3>
+          <ul class="list-inline card-meta d-flex align-items-center mb-3">
+            <li class="list-inline-item d-flex align-items-center">
+              <img
+                src="https://reflectoring.io/images/authors/ajibade_hu311bbdf7993e90112ceb55987250c547_351348_40x0_resize_q90_h2_box_3.webp"
+                alt="Olaoluwa Ajibade"
+                class="card-avatar rounded-circle me-2 imgPofile"
+                width="20"
+                height="20"
+              />
+              <a
+                href="/authors/ajibade"
+                class="text-decoration-none text-reset"
+                >{{ topic.autour }}</a
+              >
+            </li>
+            <li class="list-inline-item d-flex align-items-center">
+              <i class="far fa-calendar-alt me-2"></i>
+              <span>{{ topic.date }}</span>
+            </li>
+          </ul>
+          <p class="card-text small mb-3">{{ topic.description }}</p>
+          <a
+            href="/tutorial-graphql-apollo-server-nodejs-mongodb/"
+            class="btn btn-outline-primary btn-sm addArticle"
+            >Read more</a
+          >
         </div>
       </div>
     </div>
@@ -39,27 +74,58 @@ export default {
 </script>
 
 <style scoped>
+.rounded-4 {
+  border-radius: 10px !important;
+}
+.w-100 {
+  width: 100% !important;
+}
+.mb-4 {
+  margin-bottom: 1.5rem !important;
+}
+.card-meta li {
+  font-size: 0.8rem;
+  font-weight: 400;
+}
 .card {
-  border: none;
-  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.1);
-  border-radius: 4%;
+  height: 100%;
+  border-radius: 10px !important;
+}
+.img-fluid {
+  max-width: 100%;
+  height: auto;
 }
 
-.card-title {
-  font-size: 1.25rem;
-  font-weight: bold;
+.login {
+  font-size: 0.7rem;
+  display: block;
+  padding: 0.1em 0.9em;
 }
-.card-text {
-  font-size: 0.9rem;
-  color: #757575;
-  margin-bottom: 1rem;
+a,
+a:hover,
+a:focus {
+  text-decoration: none;
 }
-/* .btn-outline-primary {
-  border-color: #007bff;
-  color: #007bff;
+
+.pt-4 {
+  padding-top: 1.5rem !important;
 }
-.btn-outline-primary:hover {
-  background-color: #007bff;
-  color: #fff;
-} */
+.mb-5 {
+  margin-bottom: 3rem !important;
+}
+.border {
+  border: 1px solid #e1e1e1 !important;
+}
+.rounded {
+  border-radius: 6px !important;
+}
+.list-inline-item {
+  padding-left: 1rem;
+  padding-top: 1rem;
+  padding-right: 1rem;
+  padding-bottom: 1rem;
+}
+.cards-style {
+  margin-bottom: 3%;
+}
 </style>
